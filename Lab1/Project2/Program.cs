c8602056a7a9e393e1fbfd6496100e0f94577366
Krish Patel <kmpatel9861@gmail.com>
2026-07-07 22:34:25 +0530

    Upload Lab1

---
+++ Lab1/Project2/Program.cs
@@ -0,0 +1,24 @@
+﻿Random rand = new Random();
+int secretNumber = rand.Next(1, 101);
+int guess;
+int attempts = 0;
+
+do
+{
+    Console.Write("Enter your guess (1-100): ");
+    guess = Convert.ToInt32(Console.ReadLine());
+    attempts++;
+
+    if (guess > secretNumber)
+    {
+        Console.WriteLine("Too high!");
+    }
+    else if (guess < secretNumber)
+    {
+        Console.WriteLine("Too low!");
+    }
+
+} while (guess != secretNumber);
+
+Console.WriteLine("Congratulations! You guessed the correct number.");
+Console.WriteLine("Total attempts: " + attempts);
---
+++ Lab1/Project3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter two Operands: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Select the operation you want to do from +,-,*,/,% :");
+
+char c = Convert.ToChar(Console.ReadLine());
+
+switch (c)
+{
+    case '+':
+        Console.WriteLine("You have selected addition and your output is : " + (a + b));
+        break;
+    case '-':
+        Console.WriteLine("You have selected subtraction and your output is : " + (a - b));
+        break;
+    case '*':
+        Console.WriteLine("You have selected Multiplication and your output is : " + (a * b));
+        break;
+    case '/':
+        Console.WriteLine("You have selected Division and your output is : " + (a / b));
+        break;
+    case '%':
+        Console.WriteLine("You have selected Modulo and your output is : " + (a % b));
+        break;
+}
